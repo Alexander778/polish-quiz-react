b1e26b749469980f91b929fd6be8857872f5999a
Kuzmenko, Oleksandr <oleksandr.kuzmenko@corteva.com>
2025-12-22 17:47:31 +0200

    update words

--- src/data/words.ts
+++ src/data/words.ts
@@ -140,5 +140,280 @@ export const words: Word[] = [
     "word": "chociaż",
     "translation": "хоча",
     "type": "conjunction"
+  },
+  {
+    "word": "dostać",
+    "translation": "отримати",
+    "type": "verb"
+  },
+  {
+    "word": "sprawdzić",
+    "translation": "перевірити",
+    "type": "verb"
+  },
+  {
+    "word": "rozmawiać",
+    "translation": "розмовляти",
+    "type": "verb"
+  },
+  {
+    "word": "spotkać",
+    "translation": "зустрічати",
+    "type": "verb"
+  },
+  {
+    "word": "odpowiedzieć",
+    "translation": "відповідати",
+    "type": "verb"
+  },
+  {
+    "word": "trzymać",
+    "translation": "тримати",
+    "type": "verb"
+  },
+  {
+    "word": "pisać",
+    "translation": "писати",
+    "type": "verb"
+  },
+  {
+    "word": "przypomnieć",
+    "translation": "нагадати",
+    "type": "verb"
+  },
+  {
+    "word": "potrzebować",
+    "translation": "потребувати",
+    "type": "verb"
+  },
+  {
+    "word": "bluza",
+    "translation": "кофта",
+    "type": "noun"
+  },
+  {
+    "word": "spodnie",
+    "translation": "штани",
+    "type": "noun"
+  },
+  {
+    "word": "skarpety",
+    "translation": "шкарпетки",
+    "type": "noun"
+  },
+  {
+    "word": "sznurowki",
+    "translation": "шнурки",
+    "type": "noun"
+  },
+  {
+    "word": "szalik",
+    "translation": "шарф",
+    "type": "noun"
+  },
+  {
+    "word": "broda",
+    "translation": "борода",
+    "type": "noun"
+  },
+  {
+    "word": "czoło",
+    "translation": "лоб",
+    "type": "noun"
+  },
+  {
+    "word": "gardło",
+    "translation": "горло",
+    "type": "noun"
+  },
+  {
+    "word": "policzek",
+    "translation": "щока",
+    "type": "noun"
+  },
+  {
+    "word": "twarz",
+    "translation": "обличчя",
+    "type": "noun"
+  },
+  {
+    "word": "usta",
+    "translation": "рот",
+    "type": "noun"
+  },
+  {
+    "word": "wargi",
+    "translation": "губи",
+    "type": "noun"
+  },
+  {
+    "word": "włosy",
+    "translation": "волосся",
+    "type": "noun"
+  },
+  {
+    "word": "znamię",
+    "translation": "родимка",
+    "type": "noun"
+  },
+  {
+    "word": "błyskawica",
+    "translation": "блискавка",
+    "type": "noun"
+  },
+  {
+    "word": "grzmot",
+    "translation": "грім",
+    "type": "noun"
+  },
+  {
+    "word": "deszcz",
+    "translation": "дощ",
+    "type": "noun"
+  },
+  {
+    "word": "chmura",
+    "translation": "хмара",
+    "type": "noun"
+  },
+  {
+    "word": "gorąnco",
+    "translation": "спека",
+    "type": "noun"
+  },
+  {
+    "word": "wiatr",
+    "translation": "вітер",
+    "type": "noun"
+  },
+  {
+    "word": "gołoledz",
+    "translation": "ожеледиця",
+    "type": "noun"
+  },
+  {
+    "word": "słoneczny",
+    "translation": "сонячний",
+    "type": "adjective"
+  },
+  {
+    "word": "pochmurny",
+    "translation": "похмурий",
+    "type": "adjective"
+  },
+  {
+    "word": "styczeń",
+    "translation": "січень",
+    "type": "noun"
+  },
+  {
+    "word": "luty",
+    "translation": "лютий",
+    "type": "noun"
+  },
+  {
+    "word": "marzec",
+    "translation": "березень",
+    "type": "noun"
+  },
+  {
+    "word": "kwiecień",
+    "translation": "квітень",
+    "type": "noun"
+  },
+  {
+    "word": "maj",
+    "translation": "травень",
+    "type": "noun"
+  },
+  {
+    "word": "czerwiec",
+    "translation": "червень",
+    "type": "noun"
+  },
+  {
+    "word": "lipiec",
+    "translation": "липень",
+    "type": "noun"
+  },
+  {
+    "word": "sierpień",
+    "translation": "серпень",
+    "type": "noun"
+  },
+  {
+    "word": "wrzesień",
+    "translation": "вересень",
+    "type": "noun"
+  },
+  {
+    "word": "październik",
+    "translation": "жовтень",
+    "type": "noun"
+  },
+  {
+    "word": "listopad",
+    "translation": "листопад",
+    "type": "noun"
+  },
+  {
+    "word": "grudzień",
+    "translation": "грудень",
+    "type": "noun"
+  },
+  {
+    "word": "poniedziałek",
+    "translation": "понеділок",
+    "type": "noun"
+  },
+  {
+    "word": "wtorek",
+    "translation": "вівторок",
+    "type": "noun"
+  },
+  {
+    "word": "środa",
+    "translation": "середа",
+    "type": "noun"
+  },
+  {
+    "word": "czwartek",
+    "translation": "четвер",
+    "type": "noun"
+  },
+  {
+    "word": "piątek",
+    "translation": "п’ятниця",
+    "type": "noun"
+  },
+  {
+    "word": "sobota",
+    "translation": "субота",
+    "type": "noun"
+  },
+  {
+    "word": "niedziela",
+    "translation": "неділя",
+    "type": "noun"
+  },
+  {
+    "word": "wiosna",
+    "translation": "весна",
+    "type": "noun"
+  },
+  {
+    "word": "lato",
+    "translation": "літо",
+    "type": "noun"
+  },
+  {
+    "word": "jesień",
+    "translation": "осінь",
+    "type": "noun"
+  },
+  {
+    "word": "zima",
+    "translation": "зима",
+    "type": "noun"
   }
 ];
